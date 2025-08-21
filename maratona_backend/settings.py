03288097fa322063fa864b8f7a76a8f1bcb7d49d
edleyjoao@gmail.com <166552739+joaoedley@users.noreply.github.com>
2025-08-21 18:14:40 -0300

    Remove PostgreSQL and use SQLite only

--- maratona_backend/settings.py
+++ maratona_backend/settings.py
@@ -81,33 +81,12 @@ WSGI_APPLICATION = 'maratona_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-import os
-
-DATABASE_URL = os.getenv('DATABASE_URL')
-
-if DATABASE_URL:
-    # PostgreSQL em produção
-    import urllib.parse as urlparse
-    url = urlparse.urlparse(DATABASE_URL)
-    
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': url.path[1:],
-            'USER': url.username,
-            'PASSWORD': url.password,
-            'HOST': url.hostname,
-            'PORT': url.port,
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    # SQLite em desenvolvimento
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
 
 
 # Password validation
